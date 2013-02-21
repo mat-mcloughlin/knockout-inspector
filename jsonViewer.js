935812471dcd6ec30521f48602c37825d0b713af
mathew mcloughlin <mathew.mcloughlin@gmail.com>
2013-02-21 20:08:10 +0000

    Initial commit

---
+++ jsonViewer.js
@@ -0,0 +1,135 @@
+﻿ko.bindingHandlers.view = {
+		jsonViewer: {},
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        this.jsonViewer = new JsonViewer(element, valueAccessor, allBindingsAccessor);
+    },
+    update: function (element, valueAccessor, allBindingsAccessor) {
+				this.jsonViewer.compare(valueAccessor);
+        var jsonHtml = viewModelFormatter(this.jsonViewer.value, jsonViewer.getRootName());
+        element.innerHTML = jsonHtml;
+        console.log('woop');
+    }
+};
+
+var JsonViewer = function (element, valueAccessor, allBindingsAccessor) {
+	  var value = {},
+    	getRootName = function () {
+       	var propertyString = 'test';
+//        propertyString = propertyString.substring(24).replace(' = __ko_value; }', '');
+        return propertyString;
+    	},
+			compare = function(newValue) {
+				recurseObject(newValue(), value);
+			},
+			recurseObject = function(obj, newObject) {
+				for (var p in obj) {
+					var unwrapped = ko.utils.unwrapObservable(obj[p]);
+					if (newObject[p]) {
+						if (newObject[p].value && (newObject[p].value != unwrapped)) 
+							newObject[p].changeCount ++;
+					}
+					else {
+						if (typeof unwrapped == 'object') {
+							newObject[p] = { };
+				  		recurseObject(unwrapped, newObject[p]);
+						}
+				  	else
+							newObject[p] = { value: unwrapped, changeCount: 0 };
+					}
+				}
+			};
+		
+
+    return {
+        getRootName: getRootName,
+				compare: compare,
+				value: value
+    };
+};
+
+var viewModelFormatter = function () {
+    var htmlEncode = function (t) {
+        return t != null ? t.toString().replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;") : '';
+    },
+    decorateWithSpan = function (value, className) {
+    	return '<span class="' + className + '">' + htmlEncode(value) + '</span>';
+    },
+		getValue = function(value) {
+			if (value.value)
+				return value;
+			else 
+				return { value: value, changeCount: 0 };
+		},
+    valueToHtml = function (value) {
+			value = getValue(value)
+			var valueType = typeof value.value, output = "";
+			
+      if (value.value == null)
+      	output += decorateWithSpan("null", "type-null");
+      else if (value.value && value.value.constructor == Array)
+        output += arrayToHtml(value.value);
+      else if (valueType == "object" && value.value.getMonth)
+        output += stringToHtml(value.value.toString()) + '<span>' + value.changeCount + '</value';
+      else if (valueType == "object")
+          output += objectToHtml(value.value);
+            else if (valueType == "number")
+                output += decorateWithSpan(value.value, "type-number") + '<span>' + value.changeCount + '</value';
+            else if (valueType == "string")
+                output += stringToHtml(value.value) + '<span>' + value.changeCount + '</value';
+            else if (valueType == "boolean")
+                output += decorateWithSpan(value.value, "type-boolean") + '<span>' + value.changeCount + '</value';
+
+            return output;
+        },
+        stringToHtml = function (value) {
+            var output = '';
+            if (/^(http|https):\/\/[^\s]+$/.test(value))
+                output += decorateWithSpan('"', "type-string") + '<a href="' + value + '">' + htmlEncode(value) + '</a>' + decorateWithSpan('"', "type-string");
+            else
+                output += decorateWithSpan('"' + value + '"', "type-string");
+            return output;
+        },
+        arrayToHtml = function (json) {
+            var i, length, output = '<div class="collapser"></div>[<span class="ellipsis"></span><ul class="array collapsible">', hasContents = false;
+            for (i = 0, length = json.length; i < length; i++) {
+                hasContents = true;
+                output += '<li><div class="hoverable">';
+                output += valueToHtml(json[i]);
+                if (i < length - 1)
+                    output += ',';
+                output += '</div></li>';
+            }
+            output += '</ul>]';
+            if (!hasContents)
+                output = "[ ]";
+            return output;
+        },
+        objectToHtml = function (json) {
+            var i, key, length, keys = Object.keys(json), output = '<div class="collapser"></div>{<span class="ellipsis"></span><ul class="obj collapsible">', hasContents = false;
+            for (i = 0, length = keys.length; i < length; i++) {
+                key = keys[i];
+                hasContents = true;
+                output += '<li><div class="hoverable">';
+                output += '<span class="property">' + htmlEncode(key) + '</span>: ';
+                output += valueToHtml(json[key]);
+                if (i < length - 1)
+                    output += ',';
+                output += '</div></li>';
+            }
+            output += '</ul>}';
+            if (!hasContents)
+                output = "{ }";
+            return output;
+        },
+        jsonToHtml = function (json, root) {
+            var output = '';
+            output += '<div class="jsonViewer">';
+						output += '<ul class="obj collapsible"><li><div class="hoverable"><span class="property">' + root + '</span>: ';
+            output += valueToHtml(json);
+						output += '</div></li></ul>';
+            output += '</div>';
+            return output;
+        }
+
+    return jsonToHtml;
+}();
---
+++ testViewModel.js
@@ -0,0 +1,48 @@
+﻿var testViewModel = ko.observable({
+    errorId: ko.observable(1),
+    errorMessage: ko.computed(function() { return 'testing'; }),
+    errorName: null,
+		test: { foo: { bar: ko.observable('test') } },
+    items: [
+        {
+            id: 1,
+            name: ko.observable("Turton Brewery Gas"),
+            endDate: ko.observable(new Date("2016-03-31T00:00:00.000Z")),
+            isTradable: true,
+            startDate: "2013-04-01T00:00:00.000Z",
+            supplier: {
+                id: 269,
+                name: "TOTAL GAS & POWER"
+            },
+            customer: {
+                id: 1,
+                name: "2gether NHS Foundation Trust"
+            },
+            utilityCategory: {
+                id: 2,
+                name: "Natural Gas ",
+                calendar: null
+            }
+        },
+        {
+            id: 2,
+            name: "McLoughlin Facial Hair Removal",
+            endDate: "2016-03-31T00:00:00.000Z",
+            isTradable: true,
+            startDate: "2013-04-01T00:00:00.000Z",
+            supplier: {
+                id: 140,
+                name: "EDF ENERGY"
+            },
+            customer: {
+                id: 5,
+                name: "Airedale NHS Trust"
+            },
+            utilityCategory: {
+                id: 1,
+                name: "Electricity ",
+                calendar: null
+            }
+        }
+    ]
+});
